1a10033a60376f511cca5478470b5bdd9e13a91a
wodydl0 <poty9798@naver.com>
2025-05-27 13:46:24 +0900

    TS-5 feat: Trade 도메인작성 및 Order 도메인 보완

--- common-db/src/main/java/knu/motoo/commondb/order/Order.java
+++ common-db/src/main/java/knu/motoo/commondb/order/Order.java
@@ -21,10 +21,14 @@ public class Order extends BaseTimeEntity {
 
     private String symbol; // 종목 코드
 
+    @Enumerated(EnumType.STRING)
     private OrderType orderType;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
+    @Enumerated(EnumType.STRING)
+    private OrderSide orderSide;
 
     @Column(nullable = false, precision = 18, scale = 2)
     private BigDecimal price;
@@ -33,15 +37,16 @@ public class Order extends BaseTimeEntity {
     private BigDecimal quantity;
 
     @ManyToOne(fetch = FetchType.LAZY)
-@JoinColumn(name = "member_id", nullable = false)
+    @JoinColumn(name = "member_id", nullable = true)
     private Member member;
 
     @Builder
     public Order(String symbol, OrderType orderType, OrderStatus orderStatus,
-                 BigDecimal price, BigDecimal quantity, Member member) {
+                 OrderSide orderSide, BigDecimal price, BigDecimal quantity, Member member) {
         this.symbol = symbol;
         this.orderType = orderType;
         this.orderStatus = orderStatus;
+        this.orderSide = orderSide;
         this.price = price;
         this.quantity = quantity;
         this.member = member;
